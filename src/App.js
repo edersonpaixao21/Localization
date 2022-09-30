2c4295cdb3bee6c724904861cc3b5c3db53b7010
edersonpaixao21 <ederson.cpds@gmail.com>
2022-09-30 12:19:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import { Box, Flex } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import List from "./components/List";
+import Map from "./components/Map";
+import { getPlacesData } from "./services/api";
+
+export default function App() {
+  const [places, setPlaces] = useState([]);
+  const [childClick, setChildClick] = useState(null);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const filtered = places?.filter((place) => Number(place?.rating) > rating);
+
+    setFilteredPlaces(filtered);
+  }, [rating]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(type, bounds?.sw, bounds?.ne).then((data) => {
+      setPlaces(data);
+      setFilteredPlaces([]);
+      setIsLoading(false);
+    });
+  }, [type, coordinates, bounds]);
+
+  return (
+    <>
+      <Header setCoordinates={setCoordinates} />
+      <Flex h="90vh">
+        <List
+          places={filteredPlaces?.length ? filteredPlaces : places}
+          childClick={childClick}
+          isLoading={isLoading}
+          setType={setType}
+          setRating={setRating}
+          rating={rating}
+          type={type}
+          setChildClick={setChildClick}
+        />
+
+        <Map
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coordinates={coordinates}
+          places={filteredPlaces?.length ? filteredPlaces : places}
+          setChildClick={setChildClick}
+        />
+      </Flex>
+    </>
+  );
+}
---
+++ src/components/List.js
@@ -0,0 +1,156 @@
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  Image,
+  Select,
+  Text,
+} from "@chakra-ui/react";
+import React, { createRef, useEffect, useState } from "react";
+import CardDetais from "./CardsDetails";
+import Animation from "../assets/loading.json";
+import top from "../assets/top.png";
+import Lottie from "react-lottie";
+
+export default function List({
+  places,
+  childClick,
+  isLoading,
+  setType,
+  setRating,
+  type,
+  rating,
+}) {
+  const [elRefs, setElRefs] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const divTop = createRef();
+
+  useEffect(() => {
+    document.getElementById("list").addEventListener("scroll", () => {
+      if (document.getElementById("list").scrollTop > 200) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    setElRefs((refs) =>
+      Array(places?.length)
+        .fill()
+        .map((_, i) => refs[i] || React.createRef())
+    );
+  }, [places]);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: Animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <Flex
+      id="list"
+      w="500px"
+      flexDirection="column"
+      maxH="100%"
+      overflow="auto"
+    >
+      {isLoading ? (
+        <Lottie options={defaultOptions} width="400px" height="400px" />
+      ) : (
+        <>
+          <FormControl
+            ref={divTop}
+            mt="10px"
+            display="flex"
+            gap="20px"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <FormLabel fontSize="14px">Tipo</FormLabel>
+              <Select
+                value={type}
+                onChange={(e) => {
+                  setType(e.target.value);
+                }}
+                w="140px"
+                h="30px"
+              >
+                <option value="restaurants">Restaurantes</option>
+                <option value="hotels">Hotéis</option>
+                <option value="attractions">Atrações</option>
+              </Select>
+            </Box>
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <FormLabel fontSize="14px">Avaliação</FormLabel>
+              <Select
+                value={rating}
+                onChange={(e) => {
+                  setRating(e.target.value);
+                }}
+                w="140px"
+                h="30px"
+              >
+                <option value="">Tudo</option>
+                <option value="3">Acima de 3.0</option>
+                <option value="4">Acima de 4.0</option>
+                <option value="4.5">Acima de 4.5</option>
+              </Select>
+            </Box>
+          </FormControl>
+          <Flex
+            zIndex={2}
+            position="fixed"
+            mt="10px"
+            w="400px"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Image
+              transition="all 0.7s ease"
+              id="btn-top"
+              onClick={() => {
+                divTop?.current?.scrollIntoView({
+                  behavior: "smooth",
+                  block: "start",
+                  inline: "start",
+                });
+              }}
+              w={visible ? "40px" : "0px"}
+              h={visible ? "40px" : "0px"}
+              src={top}
+            />
+          </Flex>
+          <Box>
+            {places?.length <= 0 ? (
+              <Text ml="20%" mt="50%" fontWeight="bold" fontSize="18px">
+                {type} not founds in your area
+              </Text>
+            ) : (
+              places?.map((place, i) => {
+                return (
+                  <Box ref={elRefs[i]} key={i}>
+                    <CardDetais
+                      refProp={elRefs[i]}
+                      places={place}
+                      selected={Number(childClick) === i}
+                      childClick={childClick}
+                      type={type}
+                    />
+                  </Box>
+                );
+              })
+            )}
+          </Box>
+        </>
+      )}
+    </Flex>
+  );
+}
